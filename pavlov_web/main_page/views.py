13be1006ba41ec7756fcdcd97b6645963e34b142
ArtemG8 <120787936+ArtemG8@users.noreply.github.com>
2024-11-03 00:47:25 +0300

    feat: add live count of views and subs by YouTube API

--- pavlov_web/main_page/views.py
+++ pavlov_web/main_page/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
+from django.http import JsonResponse
+import requests
 
-# Create your views here.
-def test(request):
-    return render(request, 'main_page/main.html')
 def tester():
     pass
+
+
+def main(request):
+    API_KEY = 'AIzaSyB94roF0VaB4CO9uFLN_xXNtPGhD7N0trg'
+    CHANNEL_ID = 'UCpeIpEAkt9rn-gmEi0h8-SA'
+
+    url = f'https://www.googleapis.com/youtube/v3/channels?part=snippet,contentDetails,statistics&id={CHANNEL_ID}&key={API_KEY}'
+
+    response = requests.get(url)
+    data = response.json()
+
+    # Проверка на наличие данных
+    if 'items' in data and len(data['items']) > 0:
+        channel_data = data['items'][0]
+        statistics = channel_data['statistics']
+    else:
+        statistics = {}
+
+    return render(request, 'main_page/main.html', {'statistics': statistics})
+
--- pavlov_web/pavlov_web/settings.py
+++ pavlov_web/pavlov_web/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main_page',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
